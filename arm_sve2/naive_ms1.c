7ff99134ab5a82cf5df8b6267df09a279888983f
SEUNGMIN LEE <slee2284@wisc.edu>
2023-11-17 17:48:03 -0600

    Add new file

---
+++ arm_sve2/naive_ms1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Algorithm 1: Naive Algorithm using OpenMP
+/*
+    N = # of input image (NImg)
+    K = # of output feature map (Nof)
+    C = # of input feature map (Nif)
+    H = size of input/output feature map (Nix/Nox)
+    W = size of input/output feature map (Niy/Noy)
+    P = (H + 2 * padding - R) / (stride + 1)
+    Q = (Q + 2 * padding - S) / (stride + 1)
+    R = Kernel height (Kx)
+    S = kernel width (Ky)
+    stride
+    padding
+
+    4 Dimensional Arrays :
+    O = output image
+    I = input image
+    W = weight
+*/
+
+int P = (H + 2 * padding - R) / (stride + 1)
+int Q = (Q + 2 * padding - S) / (stride + 1)
+
+// pragma openmp 
+
+for (int n = 0; n < N; n++) {
+    for (int k = 0; k < K; k++) {
+        for (int c = 0; c < C; c++) {
+            for (int oj = 0; oj < P; oj++) {
+                for (int oi = 0; oi < Q; oi++) {
+                    int ij = stride * oj - padding;
+                    int ii = strid * oi - padding;
+                    for (int r = 0; r < R; r++) {
+                        for (int s = 0; s < S; s++) {
+                            O[n][k][oj][oi] += I[n][c][ij + r][ii + s] ∗ W[k][c][r][s]
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
