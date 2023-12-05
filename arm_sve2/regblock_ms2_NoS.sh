11488556f28b2cd49085aa5ae666d44fc4f622a5
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-04 18:23:19 -0600

    Update file regblock_ms2_NoS.sh

--- arm_sve2/regblock_ms2_NoS.sh
+++ arm_sve2/regblock_ms2_NoS.sh
@@ -7,4 +7,4 @@ g++ -g -o regblock_layer -O3 regblock_ms2.cpp -std=c++11
 perf record -g -- ./regblock_layer 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
 
 # Analyze the collected data, focusing on the specific function
-perf report --symbol=naive_conv_fp
+perf report --symbol=arm_sve_conv_fp
