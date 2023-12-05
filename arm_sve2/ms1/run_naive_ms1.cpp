875cb4a4659410fe0925cda9e606b3a286cc11c1
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-05 08:40:30 -0600

    Update file run_naive_ms1.cpp

--- arm_sve2/ms1/run_naive_ms1.cpp
+++ arm_sve2/ms1/run_naive_ms1.cpp
@@ -597,12 +597,16 @@ int main (int argc, char** argv) {
         }
     }
     cout << "Error Count: " << error_count << "/" << outputSize << "\n";
-    
-    // for (int i = 0; i < outputSize; i++) {
-    //     cout << naive_output[i] << endl;
-    // }
-    // cout << endl;
 
+    for (int i = 0; i < outputSize; i++) {
+        cout << naive_output[i] << endl;
+    }
+    cout << endl;
+    cout << endl;
+    for (int i = 0; i < outputSize; i++) {
+        cout << naive_output_save[i] << endl;
+    }
+    cout << endl;
     //*/
     /*
     cout << "##########################################\n";
