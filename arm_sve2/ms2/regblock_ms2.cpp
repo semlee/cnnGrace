1847f9c23424a78cbfdf2adf46d7b21dd120b0ae
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-04 22:48:00 -0600

    Update file regblock_ms2.cpp

--- arm_sve2/ms2/regblock_ms2.cpp
+++ arm_sve2/ms2/regblock_ms2.cpp
@@ -10,36 +10,6 @@
 #endif
 // #include <arm_sve.h>
 
-
-typedef struct {
-  int nImg;
-  int nIfm;
-  int nOfm;
-  int ifhp;
-  int ifwp;
-  int ifh;
-  int ifw;
-  int ofhp;
-  int ofwp;
-  int ofh;
-  int ofw;
-  int pad_h;
-  int pad_w;
-  int pad_h_in;
-  int pad_w_in;
-  int pad_h_out;
-  int pad_w_out;
-  int kh;
-  int kw;
-  int stride_h;
-  int stride_w;
-  int VLEN;
-  int RB_p;
-  int RB_q;
-} conv_t;
-
-
-
 void arm_sve_conv_fp(conv_t* param, const float* input, float* output, const float* filter, const float* bias) {
     // Fetch data from param struct
     int N         = param->nImg;
