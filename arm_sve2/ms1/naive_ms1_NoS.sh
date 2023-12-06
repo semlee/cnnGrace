9af5b130e3c195ab8e5a16a6a25bae7becfc5731
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-06 14:16:09 -0600

    Update file naive_ms1_NoS.sh

--- arm_sve2/ms1/naive_ms1_NoS.sh
+++ arm_sve2/ms1/naive_ms1_NoS.sh
@@ -7,6 +7,13 @@ g++ -O3 run_naive_ms1.cpp -o naive_layer_naive -std=c++11
 
 # objdump -D -Mintel naive_layer | grep "vex.v"
 
+echo "-O3"
 ./naive_layer_naive 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
+echo "-mavx2 -O3"
 ./naive_layer_avx2 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
+echo "-march=native -mtune=native"
 ./naive_layer2_native 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
+
+rm naive_layer_naive
+rm naive_layer_avx2
+rm naive_layer2_native
