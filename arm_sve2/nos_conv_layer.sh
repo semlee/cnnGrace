5121d37d25fcc18653eca1978b9edcaf13161924
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-10 01:51:55 -0600

    Update file nos_conv_layer.sh

--- arm_sve2/nos_conv_layer.sh
+++ arm_sve2/nos_conv_layer.sh
@@ -11,7 +11,8 @@ g++ main.o naive_ms1.o regblock_ms2.o -o conv_layer -O3 -std=c++11
 # Run with perf, focusing on a specific function
 # perf record -g -- 
 
-./conv_layer 1000 16 16 32 32 64 3 3 0 0 1 'F' 'L' 0
+#./conv_layer iters ifw ifh nImg nIfm nOfm kw kh padw padh stride type format padding_mode
+./conv_layer 1000 16 16 5 32 64 3 3 1 1 1 'F' 'L' 0
 
 rm naive_ms1.o 
 rm regblock_ms2.o
