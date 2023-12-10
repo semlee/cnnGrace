3e8dffbdc1afff6eec6b2a24907928d5ad797ffb
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-10 01:16:45 -0600

    Update 2 files
    - /arm_sve2/naive_ms1.c
    - /arm_sve2/ms2/regblock_ms2_NoS.sh

--- arm_sve2/ms2/regblock_ms2_NoS.sh
+++ arm_sve2/ms2/regblock_ms2_NoS.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
-# Compile with debugging information
-g++ -g -o regblock_layer -O3 regblock_ms2.cpp -std=c++11
+# # Compile with debugging information
+# g++ -g -o regblock_layer -O3 regblock_ms2.cpp -std=c++11
 
-# Run with perf, focusing on a specific function
-perf record -g -- ./regblock_layer 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
+# # Run with perf, focusing on a specific function
+# perf record -g -- ./regblock_layer 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
 
-# Analyze the collected data, focusing on the specific function
+# # Analyze the collected data, focusing on the specific function
 
-perf annotate --symbol=arm_sve_conv_fp
+# perf annotate --symbol=arm_sve_conv_fp
 
-sudo perf stat -e cache-references,cache-misses,instructions,cycles,branches,branch-misses -- ./regblock_layer 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
+# sudo perf stat -e cache-references,cache-misses,instructions,cycles,branches,branch-misses -- ./regblock_layer 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
 
-perf stat -e instructions,cycles,branches,branch-misses -- ./your_binary 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
+# perf stat -e instructions,cycles,branches,branch-misses -- ./your_binary 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
 
-perf record -e branch-misses -- ./your_binary 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
-perf report
+# perf record -e branch-misses -- ./your_binary 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
+# perf report
 
 
-perf record -e cycles -c 1 -ag -- ./regblock_layer 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
+# perf record -e cycles -c 1 -ag -- ./regblock_layer 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
+
+g++ -mavx2 -O3 run_regblock_ms2.cpp -o reg_layer_avx2 -std=c++11
+# g++ -o reg_layer_native -march=native -mtune=native -O3 run_regblock_ms2.cpp -std=c++11
+# g++ -O3 run_regblock_ms2.cpp -o reg_layer_naive -std=c++11
+
+# objdump -D -Mintel naive_layer | grep "vex.v"
+
+# echo "-O3"
+# ./naive_layer_naive 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
+echo "-mavx2 -O3"
+./naive_layer_avx2 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
+# echo "-march=native -mtune=native"
+# ./naive_layer2_native 1000 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
+
+# rm naive_layer_naive
+rm naive_layer_avx2
+# rm naive_layer2_native
