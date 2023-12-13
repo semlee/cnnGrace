cc3ea3ad1f7978640826bb81d404242bda8a696d
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-13 03:30:23 -0600

    Update file regblock_ms2_NoS.sh

--- arm_sve2/ms2/regblock_ms2_NoS.sh
+++ arm_sve2/ms2/regblock_ms2_NoS.sh
@@ -29,7 +29,7 @@ g++ -g -O3 run_regblock_ms2.cpp -o reg_layer_naive -std=c++11
 # objdump -D -Mintel naive_layer | grep "vex.v"
 
 echo "-O3"
-./reg_layer_naive 1000 16 16 1 5 5 3 3 1 1 1 'F' 'L' 0
+./reg_layer_naive 1000 16 16 5 5 64 3 3 1 1 1 'F' 'L' 0
 # echo "-mavx2 -O3"
 # ./reg_layer_avx2 10 16 16 1 3 64 3 3 1 1 1 'F' 'L' 0
 # echo "-march=native -mtune=native"
