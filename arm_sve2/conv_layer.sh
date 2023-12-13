4acac6f1f7ed0c25c40abdc581211630e3993dae
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-13 09:55:54 -0600

    Update 2 files
    - /arm_sve2/conv_layer.sh
    - /arm_sve2/nos_conv_layer.sh

--- arm_sve2/conv_layer.sh
+++ arm_sve2/conv_layer.sh
@@ -23,15 +23,16 @@ type='F'
 format='L'
 padding_mode=1
 
+export OMP_NUM_THREADS = 72
 # Compile individual source files
-g++ -c main.cpp -o main.o -std=c++11
-g++ -c ms1/naive_ms1.cpp -o naive_ms1.o -std=c++11
-g++ -c ms2/regblock_ms2.cpp -o regblock_ms2.o -std=c++11
+g++ -c main.cpp -o main.o -fopenmp -std=c++11
+g++ -c ms1/naive_ms1.cpp -o naive_ms1.o -fopenmp -std=c++11
+g++ -c ms2/regblock_ms2.cpp -o regblock_ms2.o -fopenmp -std=c++11
 # g++ -c ms3/regsve_ms3.cpp -o regsve_ms3.o -march=native -std=c++11
 
 # Link object files to create the executable
 # g++ main.o naive_ms1.o regblock_ms2.o regsve_ms3.o -o conv_layer -march=native -O3 -std=c++11
-g++ main.o naive_ms1.o regblock_ms2.o -o conv_layer -O3 -std=c++11
+g++ main.o naive_ms1.o regblock_ms2.o -o conv_layer -O3 -fopenmp -std=c++11
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
--- arm_sve2/nos_conv_layer.sh
+++ arm_sve2/nos_conv_layer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Compile individual source files
-g++ -c main.cpp -o main.o -std=c++11
-g++ -c ms1/naive_ms1.cpp -o naive_ms1.o -std=c++11
-g++ -c ms2/regblock_ms2.cpp -o regblock_ms2.o -std=c++11
+g++ -c main.cpp -o main.o -fopenmp -std=c++11
+g++ -c ms1/naive_ms1.cpp -o naive_ms1.o -fopenmp -std=c++11
+g++ -c ms2/regblock_ms2.cpp -o regblock_ms2.o -fopenmp -std=c++11
 # g++ -c ms3/regsve_ms3.cpp -o regsve_ms3.o -march=native -std=c++11
 
 # Link object files to create the executable
 # g++ main.o naive_ms1.o regblock_ms2.o regsve_ms3.o -o conv_layer -march=native -O3 -std=c++11
-g++ main.o naive_ms1.o regblock_ms2.o -o conv_layer -O3 -std=c++11
+g++ main.o naive_ms1.o regblock_ms2.o -o conv_layer -O3 -fopenmp -mavx2 -std=c++11
 
 # Run with perf, focusing on a specific function
 # perf record -g -- 
