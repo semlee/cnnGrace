3ac2182e08ce0c6d34971e067f8524c9966db418
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-13 01:59:08 -0600

    Update 2 files
    - /arm_sve2/conv_layer.sh
    - /arm_sve2/main.cpp

--- arm_sve2/conv_layer.sh
+++ arm_sve2/conv_layer.sh
@@ -26,12 +26,12 @@ padding_mode=0
 # Compile individual source files
 g++ -c main.cpp -o main.o -std=c++11
 g++ -c ms1/naive_ms1.cpp -o naive_ms1.o -std=c++11
-# g++ -c ms2/regblock_ms2.cpp -o regblock_ms2.o -std=c++11
-g++ -c ms3/regsve_ms3.cpp -o regsve_ms3.o -march=native -std=c++11
+g++ -c ms2/regblock_ms2.cpp -o regblock_ms2.o -std=c++11
+# g++ -c ms3/regsve_ms3.cpp -o regsve_ms3.o -march=native -std=c++11
 
 # Link object files to create the executable
 # g++ main.o naive_ms1.o regblock_ms2.o regsve_ms3.o -o conv_layer -march=native -O3 -std=c++11
-g++ main.o naive_ms1.o regsve_ms3.o -o conv_layer -O3 -std=c++11
+g++ main.o naive_ms1.o regblock_ms2.o -o conv_layer -O3 -std=c++11
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
@@ -39,7 +39,7 @@ if [ $? -eq 0 ]; then
 
     # Run the compiled program with command-line arguments using srun
     #./naive_ms1 iters ifw ifh nImg nIfm nOfm kw kh padw padh stride type format padding_mode
-    srun -N 1 -p cg1-high --exclusive ./conv_layer $iters $ifw $ifh $nImg $nIfm $nOfm $kw $kh $padw $padh $VLEN $RB_p $RB_q $stride $type $format $padding_mode
+    srun -N 1 -p -high --exclusive ./conv_layer $iters $ifw $ifh $nImg $nIfm $nOfm $kw $kh $padw $padh $VLEN $RB_p $RB_q $stride $type $format $padding_mode
 
     # Optionally, you can pass command-line arguments stored in a file
     # srun -N 1 -p cg1-high --exclusive ./naive_layer $(cat input_args.txt)
@@ -55,6 +55,6 @@ fi
 
 rm main.o
 rm naive_ms1.o
-# rm regblock_ms2.o
-rm regsve_ms3.o
+rm regblock_ms2.o
+# rm regsve_ms3.o
 rm conv_layer
