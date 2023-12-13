81a2a9c33455c477d919ec1fd491fec55331b3e7
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-13 03:14:00 -0600

    Update 2 files
    - /arm_sve2/conv_layer.sh
    - /arm_sve2/main.cpp

--- arm_sve2/conv_layer.sh
+++ arm_sve2/conv_layer.sh
@@ -24,14 +24,14 @@ format='L'
 padding_mode=0
 
 # Compile individual source files
-g++ -c main.cpp -o main.o -march=native -std=c++11
+g++ -c main.cpp -o main.o -std=c++11
 g++ -c ms1/naive_ms1.cpp -o naive_ms1.o -std=c++11
-# g++ -c ms2/regblock_ms2.cpp -o regblock_ms2.o -std=c++11
-g++ -c ms3/regsve_ms3.cpp -o regsve_ms3.o -march=native -std=c++11
+g++ -c ms2/regblock_ms2.cpp -o regblock_ms2.o -std=c++11
+# g++ -c ms3/regsve_ms3.cpp -o regsve_ms3.o -march=native -std=c++11
 
 # Link object files to create the executable
 # g++ main.o naive_ms1.o regblock_ms2.o regsve_ms3.o -o conv_layer -march=native -O3 -std=c++11
-g++ main.o naive_ms1.o regsve_ms3.o -o conv_layer -march=native -O3 -std=c++11
+g++ main.o naive_ms1.o regblock_ms2.o -o conv_layer -O3 -std=c++11
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
@@ -55,6 +55,6 @@ fi
 
 rm main.o
 rm naive_ms1.o
-# rm regblock_ms2.o
-rm regsve_ms3.o
+rm regblock_ms2.o
+# rm regsve_ms3.o
 rm conv_layer
