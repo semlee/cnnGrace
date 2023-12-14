e3172cc38df8a5f82f3b74e1b710e2681a68fa36
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-14 03:33:57 -0600

    Update 2 files
    - /arm_sve2/ms1/result.csv
    - /arm_sve2/ms1/run_resnet50.sh

--- arm_sve2/ms1/run_resnet50.sh
+++ arm_sve2/ms1/run_resnet50.sh
@@ -9,7 +9,7 @@
 # Compile individual source files
 
 export OMP_NUM_THREADS=72
-g++ -o conv_layer -O3 -fopenmp run_naive_ms1.cpp -std=c++11
+g++ -o conv_layer -O0 -fopenmp run_naive_ms1.cpp -std=c++11
 
 ITERS=10
 MB=72
