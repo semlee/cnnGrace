3bd3482a5fc7e52c0a8075170334dcab9412acea
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-15 11:34:17 -0600

    Update file nos_conv_layer.sh

--- arm_sve2/nos_conv_layer.sh
+++ arm_sve2/nos_conv_layer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Compile individual source files
-g++ -c main.cpp -o main.o -fopenmp -std=c++11
-g++ -c ms1/naive_ms1.cpp -o naive_ms1.o -fopenmp -std=c++11
-g++ -c ms2/regblock_ms2.cpp -o regblock_ms2.o -fopenmp -std=c++11
+g++ -g -c main.cpp -o main.o -fopenmp -std=c++11
+g++ -g -c ms1/naive_ms1.cpp -o naive_ms1.o -fopenmp -std=c++11
+g++ -g -c ms2/regblock_ms2.cpp -o regblock_ms2.o -fopenmp -std=c++11
 # g++ -c ms3/regsve_ms3.cpp -o regsve_ms3.o -march=native -std=c++11
 
 # Link object files to create the executable
@@ -16,7 +16,7 @@ g++ -g main.o naive_ms1.o regblock_ms2.o -o conv_layer -O3 -fopenmp -mavx2 -std=
 #./conv_layer iters ifw ifh nImg nIfm nOfm kw kh padw padh stride type format padding_mode
 ./conv_layer 1000 224 224 10 3 64 7 7 3 3 2 4 7 7 'F' 'L' 1
 
-rm naive_ms1.o 
-rm regblock_ms2.o
-rm main.o
+# rm naive_ms1.o 
+# rm regblock_ms2.o
+# rm main.o
 # rm conv_layer
