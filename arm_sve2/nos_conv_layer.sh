216f36e5942814f326c124ae937764c74b004a44
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-15 12:00:30 -0600

    Update file nos_conv_layer.sh

--- arm_sve2/nos_conv_layer.sh
+++ arm_sve2/nos_conv_layer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Compile individual source files
-g++ -g -c main.cpp -o main.o -fopenmp -std=c++11
-g++ -g -c ms1/naive_ms1.cpp -o naive_ms1.o -fopenmp -std=c++11
-g++ -g -c ms2/regblock_ms2.cpp -o regblock_ms2.o -fopenmp -std=c++11
+g++ -g -Wall -c main.cpp -o main.o -fopenmp -std=c++11
+g++ -g -Wall -c ms1/naive_ms1.cpp -o naive_ms1.o -fopenmp -std=c++11
+g++ -g -Wall -c ms2/regblock_ms2.cpp -o regblock_ms2.o -fopenmp -std=c++11
 # g++ -c ms3/regsve_ms3.cpp -o regsve_ms3.o -march=native -std=c++11
 
 # Link object files to create the executable
 # g++ main.o naive_ms1.o regblock_ms2.o regsve_ms3.o -o conv_layer -march=native -O3 -std=c++11
-g++ -g main.o naive_ms1.o regblock_ms2.o -o conv_layer -O3 -fopenmp -mavx2 -std=c++11
+g++ -g -Wall main.o naive_ms1.o regblock_ms2.o -o conv_layer -O3 -fopenmp -mavx2 -std=c++11
 
 # Run with perf, focusing on a specific function
 # perf record -g -- 
