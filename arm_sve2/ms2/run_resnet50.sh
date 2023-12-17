30934947a62e52b320fb798e6bd80d823974e337
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-17 16:46:43 -0600

    Update 3 files
    - /arm_sve2/ms3/run_resnet50.sh
    - /arm_sve2/ms2/run_resnet50.sh
    - /arm_sve2/ms1/run_resnet50.sh

--- arm_sve2/ms2/run_resnet50.sh
+++ arm_sve2/ms2/run_resnet50.sh
@@ -4,16 +4,16 @@
 #SBATCH -J run_resnet50
 #SBATCH -o output-%j.out -e output-%j.err
 #SBATCH -N 1
-#SBATCH -c 1
+#SBATCH -c 72
 #SBATCH -p cg1-cpu480gb-gpu96gb
 
 # Compile the C++ file
 # Compile individual source files
-# export OMP_NUM_THREADS=72
-g++ -o conv_layer -O3 run_regblock_ms2.cpp -std=c++11
+export OMP_NUM_THREADS=72
+g++ -o conv_layer -O3 -fopenmp run_regblock_ms2.cpp -std=c++11
 
 ITERS=10
-MB=1
+MB=72
 TYPE='F'
 FORMAT='L'
 PAD=1
@@ -35,7 +35,7 @@ stride_values=(2 1 1 1 1 2 2 1 1 1 2 2 1 1 1 2 2 1 1 1)
 
 # Iterate over the indices of the arrays
 for i in "${!ifw_values[@]}"; do
-    taskset 0x00000001 srun -N 1 --exclusive ./conv_layer \
+    srun -N 1 --exclusive ./conv_layer \
         $ITERS ${ifw_values[$i]} ${ifh_values[$i]} $nImg_values ${nIfm_values[$i]} ${nOfm_values[$i]} \
         ${kw_values[$i]} ${kh_values[$i]} ${padw_values[$i]} ${padh_values[$i]} ${stride_values[$i]} \
         $VLEN $RB_p $RB_q $TYPE $FORMAT $PAD
