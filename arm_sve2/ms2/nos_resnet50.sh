b09bf16785abe082f47ce1e6eaf7e23d54d5ac0f
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-16 22:54:08 -0600

    Update 2 files
    - /arm_sve2/ms2/nos_resnet50.sh
    - /arm_sve2/ms1/nos_resnet50.sh

--- arm_sve2/ms2/nos_resnet50.sh
+++ arm_sve2/ms2/nos_resnet50.sh
@@ -30,7 +30,7 @@ output_file="results.txt"
 
 # Iterate over the indices of the arrays
 for i in "${!ifw_values[@]}"; do
-    taskset -c 0 ./conv_layer \
+    ./conv_layer \
         $ITERS ${ifw_values[$i]} ${ifh_values[$i]} $nImg_values ${nIfm_values[$i]} ${nOfm_values[$i]} \
         ${kw_values[$i]} ${kh_values[$i]} ${padw_values[$i]} ${padh_values[$i]} ${stride_values[$i]} \
         $VLEN $RB_p $RB_q $TYPE $FORMAT $PAD \
