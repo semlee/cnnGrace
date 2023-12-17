389a3479e397287f4d4523c749ef9ac930d5eb74
SEUNGMIN LEE <slee2284@wisc.edu>
2023-12-16 21:15:26 -0600

    Update file run_regblock_ms2.cpp

--- arm_sve2/ms2/run_regblock_ms2.cpp
+++ arm_sve2/ms2/run_regblock_ms2.cpp
@@ -48,7 +48,96 @@ typedef struct {
   int RB_q;
 } conv_t;
 
-void arm_sve_conv_fp(conv_t* param, const float* input, float* output, const float* filter, const float* bias) {
+void CONV(const float* input, float* output, const float* filter, int kh, int kw, int ifh, int ifw, int ofm_b, int ifm_b, int nOfm, int nIfm, int VLEN, int RB_p, int RB_q, int oj, int oi, int ij, int ii, int ifwp, int ofwp, int stride_h, int stride_w) {
+    int kj, ki, ofm, ifm, p, q, ij0, ii0;
+    for (kj = 0; kj < kh; ++kj) { // R
+        if (ij + kj < 0 || ij + kj >= ifh) continue;
+        for (ki = 0; ki < kw; ++ki) { // S
+            if (ii + ki < 0 || ii + ki >= ifw) continue;
+            for (ofm = 0; ofm < VLEN && ofm_b * VLEN + ofm < nOfm; ofm++) {
+                for (ifm = 0; ifm < VLEN && ifm_b * VLEN + ifm < nIfm; ifm++) {
+                    size_t filterIndex = kj * kw * VLEN * VLEN +
+                                         ki * VLEN * VLEN +
+                                         ifm * VLEN +
+                                         ofm;
+                    for (p = 0; p < RB_p; p++) {
+                        ij0 = ij + stride_h * p;
+                        if (ij0 + kj < 0 || ij0 + kj >= ifh) continue;
+                        for (q = 0; q < RB_q; q++) {
+                            ii0 = ii + stride_w * q;
+                            if (ii0 + ki < 0 || ii0 + ki >= ifw) continue;
+                            size_t inputIndex = (ij0 + kj) * ifwp * VLEN +
+                                                (ii0 + ki) * VLEN +
+                                                ifm;
+                            size_t outputIndex = (oj + p) * ofwp * VLEN +
+                                                 (oi + q) * VLEN +
+                                                 ofm;
+
+                            output[outputIndex] += filter[filterIndex] * input[inputIndex];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void reg_block_conv_fp_mod(conv_t* param, const float* input, float* output, const float* filter, const float* bias) {
+    // Fetch data from param struct
+    int nImg = param->nImg;
+    int nIfm = param->nIfm;
+    int nOfm = param->nOfm;
+    int ifhp = param->ifhp;
+    int ifwp = param->ifwp;
+    int ofhp = param->ofhp;
+    int ofwp = param->ofwp;
+    int ifh = param->ifh;
+    int ifw = param->ifw;
+    int ofh = param->ofh;
+    int ofw = param->ofw;
+    int pad_h = param->pad_h;
+    int pad_w = param->pad_w;
+    int pad_h_in = param->pad_h_in;
+    int pad_w_in = param->pad_w_in;
+    int pad_h_out = param->pad_h_out;
+    int pad_w_out = param->pad_w_out;
+    int kh = param->kh;
+    int kw = param->kw;
+    int stride_h = param->stride_h;
+    int stride_w = param->stride_w;
+    int VLEN = param->VLEN;
+    int RB_p = param->RB_p;
+    int RB_q = param->RB_q;
+
+    int nIfm_b = nIfm / VLEN + (nIfm % VLEN != 0);
+    int nOfm_b = nOfm / VLEN + (nOfm % VLEN != 0);
+    int ofh_b = ofh / RB_p;
+    int ofw_b = ofw / RB_q;
+    int img, ofm_b, ifm_b, oj_b, oj, ij, oi_b, oi, ii, kj, ki, ofm, ifm, p, q, ij0, ii0;
+
+    for (img = 0; img < nImg; ++img) { // N
+        for (ofm_b = 0; ofm_b < nOfm_b; ofm_b++) { // K
+            for (ifm_b = 0; ifm_b < nIfm_b; ifm_b++) { // C
+                for (oj_b = 0; oj_b < ofh_b; ++oj_b) { // P
+                    for (oi_b = 0; oi_b < ofw_b; ++oi_b) { // Q
+                        oj = oj_b * RB_p;
+                        ij = oj * stride_h - pad_h;
+                        oi = oi_b * RB_q;
+                        ii = oi * stride_w - pad_w;
+                        auto inputIndex = img * nIfm_b * ifhp * ifwp * VLEN + ifm_b * ifhp * ifwp * VLEN;
+                        auto outputIndex = img * nOfm_b * ofhp * ofwp * VLEN + ofm_b * ofhp * ofwp * VLEN;
+                        auto filterIndex = ofm_b * nIfm_b * kh * kw * VLEN * VLEN + ifm_b * kh * kw * VLEN * VLEN;
+                        auto subvecSize = kh * kw * VLEN * VLEN * RB_p * RB_q;
+                        CONV(input + inputIndex, output + outputIndex, filter + filterIndex, kh, kw, ifh, ifw, ofm_b, ifm_b, nOfm, nIfm, VLEN, RB_p, RB_q, oj, oi, ij, ii, ifwp, ofwp, stride_h, stride_w);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+void arm_sve_conv_fp_mod(conv_t* param, const float* input, float* output, const float* filter, const float* bias) {
     // Fetch data from param struct
     int nImg      = param->nImg;
     int nIfm      = param->nIfm;
